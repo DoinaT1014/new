94367ae7b4865f5950f3ac9bcb628db57edbbb88
doina <doina.talpaaapp@gmail.com>
2017-06-08 20:51:02 +0300

    add

---
+++ assets/insert.php
@@ -0,0 +1,20 @@
+<?php
+ 
+require 'connection.php';
+$conn    = Connect();
+$uname   = $conn->real_escape_string($_POST['uname']);
+$psw  = $conn->real_escape_string($_POST['psw']);
+$query   = "INSERT into logare (uname,psw) VALUES('" . $uname . "','" . $psw . "')";
+$success = $conn->query($query);
+ 
+if (!$success) {
+    die("Opss ,nu s-a facut conexiunea la baza de date: ".$conn->error);
+ 
+}
+ 
+echo "Conexiunea este reusita  <br>";
+ 
+$conn->close();
+header('Location: practica.html'); 
+ 
+?>
